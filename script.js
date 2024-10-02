25bbd22c0ea7af538e5dab4343975781e3d7b0e1
jawadahmed334 <jawadahmed2435@gmail.com>
2024-10-02 18:28:58 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Select all flowers
-const flowers = document.querySelectorAll('.flower');
-
-// Add click event to each flower
-flowers.forEach(flower => {
-  flower.addEventListener('click', () => {
-    flower.classList.toggle('blooming');
-  });
-});
